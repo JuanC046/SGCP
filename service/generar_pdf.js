ee2bdc820bab26b4176598f512cf53b4cfb3fd5a
juanc <juan.lopez15@utp.edu.co>
2023-10-07 21:10:02 -0500

    servicio para generar el pdf

---
+++ service/generar_pdf.js
@@ -0,0 +1,36 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const Handlebars = require("handlebars"); // Agrega esta línea
+
+// Datos del archivo JSON (reemplaza 'datos.json' con tu ruta y nombre de archivo)
+const datos = require("../datos.json");
+
+const generarPdf = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Lee y carga la plantilla HTML (reemplaza 'plantilla.html' con tu ruta y nombre de archivo)
+  const html = fs.readFileSync("./comprobantePago.html", "utf-8");
+
+  // Compila la plantilla Handlebars
+  const template = Handlebars.compile(html);
+
+  // Renderiza la plantilla con los datos
+  const htmlConDatos = template({ datos });
+
+  // Establece el contenido HTML de la página
+  await page.setContent(htmlConDatos);
+
+  // Genera el PDF
+  await page.pdf({
+    path: "comprobante_pago.pdf",
+    format: "A4",
+    landscape: true, // Establece la orientación en horizontal
+  });
+
+  await browser.close();
+};
+
+module.exports = {
+  generarPdf,
+};
